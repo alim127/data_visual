8c2f2e9e837452051e46139c0483436a849212cc
alim127 <audreylim127@gmail.com>
2021-01-14 20:22:09 -0500

    climate

---
+++ climate_change_vis.py
@@ -0,0 +1,43 @@
+from time import time
+from pandas.io.parsers import read_csv
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import csv
+
+dt = 'dt'
+lat = 'LandAverageTemperature'
+lat2 = 'landaveragetemperature'
+
+st.title("Global Temperature Change from 1750-2015")
+
+climate_url = ('C:/Users/audrey/Documents/Datasets/climate_change_temp_change/GlobalTemperatures.csv')
+
+
+
+
+@st.cache
+def load_data_climate():
+    data = pd.read_csv(climate_url, usecols=[dt, lat])
+    data.dropna(inplace=True)
+    
+    return data
+
+temp_data = load_data_climate()
+ 
+
+
+if st.checkbox('Show raw data for historic global temperature'):
+    st.write(temp_data)
+
+
+
+
+
+
+
+
+
+
+
